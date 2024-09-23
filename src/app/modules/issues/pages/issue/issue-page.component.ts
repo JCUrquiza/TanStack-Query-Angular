9bde543b70d4990815a45a8bb9611e106adc0db8
Juan Carlos <gurquiza8@gmail.com>
2024-09-23 11:14:24 -0600

    feature: design of issues list

---
+++ src/app/modules/issues/pages/issue/issue-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'issue-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './issue-page.component.html',
+  styles: ``
+})
+export default class IssuePageComponent {
+
+}
---
+++ src/app/modules/issues/pages/issues-list/issues-list-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'issues-list-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './issues-list-page.component.html',
+  styles: ``,
+})
+export default class IssuesListPageComponent {
+
+}
