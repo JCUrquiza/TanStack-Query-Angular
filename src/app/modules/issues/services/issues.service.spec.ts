d9e5a7e66c32003587fe7a7cc85f27f892951f5f
Juan Carlos <gurquiza8@gmail.com>
2024-09-29 16:00:38 -0600

    feature:test. issuesService refetch of tamStackQuery for length and body of response

--- src/app/modules/issues/services/issues.service.spec.ts
+++ src/app/modules/issues/services/issues.service.spec.ts
@@ -1,17 +1,39 @@
-import { TestBed } from "@angular/core/testing";
-import { IssuesService } from "./issues.service";
+import { TestBed } from '@angular/core/testing';
+import { provideAngularQuery, QueryClient } from '@tanstack/angular-query-experimental';
+import { IssuesService } from './issues.service';
 
 describe(`IssuesService`, () => {
 
   let service: IssuesService;
+  const queryClient = new QueryClient();
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      teardown: {
+        destroyAfterEach: false,
+      },
+      providers: [ provideAngularQuery(queryClient) ]
+    });
     service = TestBed.inject(IssuesService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should load labels', async() => {
+    const { data } = await service.labelsQuery.refetch();
+    expect(data?.length).toBe(30);
+
+    const [label] = data!;
+    // console.log(label);
+    expect(typeof label.color).toBe('string');
+    expect(typeof label.default).toBe('boolean');
+    expect(typeof label.description).toBe('string');
+    expect(typeof label.id).toBe('number');
+    expect(typeof label.name).toBe('string');
+    expect(typeof label.node_id).toBe('string');
+    expect(typeof label.url).toBe('string');
   });
 
 });
